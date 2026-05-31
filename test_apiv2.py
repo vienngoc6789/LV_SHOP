09f85fc1dddc7ee179dac9275ccd283ace6cec14
vienngoc6789 <ngocdz6996@gmail.com>
2026-05-31 18:56:14 +0700

    Update test_apiv2.py

--- test_apiv2.py
+++ test_apiv2.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAogSu_14gq5g45wJS062N2s27Rmr7Fx7c")
+client = genai.Client(api_key="??DOBIETDAY")
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
