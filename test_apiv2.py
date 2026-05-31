91a98c4cafe193e203bad19aa9a904dca7aa6084
Lương Văn Ngọc <ngocdz6996@gmail.com>
2026-05-31 19:18:23 +0700

    Delete test_apiv2.py

--- test_apiv2.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="??DOBIETDAY")
-
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
-)
-print(response.text)
